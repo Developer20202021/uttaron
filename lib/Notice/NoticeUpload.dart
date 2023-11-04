cb92bc45442624f1274dcec8d215bb81da2cd4e7
Developer20202021 <mahadikaushik8888@gmail.com>
2023-11-04 10:24:18 +0600

    Done

---
+++ lib/Notice/NoticeUpload.dart
@@ -0,0 +1,350 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:http/http.dart' as http;
+import 'package:uuid/uuid.dart';
+
+
+
+
+class NoticeUpload extends StatefulWidget {
+  const NoticeUpload({super.key});
+
+  @override
+  State<NoticeUpload> createState() => _NoticeUploadState();
+}
+
+class _NoticeUploadState extends State<NoticeUpload> {
+
+    var uuid = Uuid();
+ 
+  TextEditingController TitleController = TextEditingController();
+  TextEditingController DescriptionController = TextEditingController();
+
+
+
+
+
+  bool loading = false;
+
+  bool buttonVisible = false;
+
+
+
+
+
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+   
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+      var NoticeID = uuid.v4();
+
+    FocusNode myFocusNode = new FocusNode();
+ 
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+        leading: IconButton(onPressed: () => Navigator.of(context).pop(), icon: Icon(Icons.chevron_left)),
+        title: const Text("Upload Notice",  style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.transparent,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        centerTitle: true,
+        
+      ),
+      body: loading?Padding(
+                padding: const EdgeInsets.only(top: 40),
+                child: Center(
+                      child: LoadingAnimationWidget.discreteCircle(
+                        color: const Color(0xFF1A1A3F),
+                        secondRingColor: Theme.of(context).primaryColor,
+                        thirdRingColor: Colors.white,
+                        size: 100,
+                      ),
+                    ),
+              ): SingleChildScrollView(
+
+        child:  Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            
+              
+         
+            
+            
+            
+              TextField(
+               
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Title',
+                     labelStyle: TextStyle(
+        color: myFocusNode.hasFocus ? Theme.of(context).primaryColor: Colors.black
+            ),
+                    hintText: 'Title',
+            
+                    //  enabledBorder: OutlineInputBorder(
+                    //       borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                    //     ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 3, color: Theme.of(context).primaryColor),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                        ),
+                    
+                    
+                    ),
+                controller: TitleController,
+              ),
+
+
+
+
+
+             
+
+
+
+              SizedBox(height: 8,),
+
+
+
+
+                  TextField(
+                
+               onChanged: (value) {
+
+
+                if (value.isEmpty || value == null) {
+
+                  setState(() {
+                    buttonVisible = false;
+                  });
+                  
+                }
+
+                else{
+
+
+                   setState(() {
+                    buttonVisible = true;
+                  });
+
+
+
+                }
+                
+
+
+
+                 print(value);
+               },
+               maxLines: 13,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Description',
+                     labelStyle: TextStyle(
+        color: myFocusNode.hasFocus ? Theme.of(context).primaryColor: Colors.black
+            ),
+                    hintText: 'Description',
+            
+                    //  enabledBorder: OutlineInputBorder(
+                    //       borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                    //     ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 3, color: Theme.of(context).primaryColor),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                        ),
+                    
+                    
+                    ),
+                controller: TitleController,
+              ),
+
+
+
+
+
+
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+              SizedBox(
+                height: 10,
+              ),
+            
+            
+            
+            
+            
+            
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(width: 150, child:buttonVisible? TextButton(onPressed: (){
+
+                    setState(() {
+                      loading = true;
+                    });
+
+                    final docUser = FirebaseFirestore.instance.collection("Notice");
+
+
+                var NoticeData ={
+
+                  "Title":TitleController.text.trim(),
+                  "Description":DescriptionController.text.trim(),
+                  "ImageLink":[],
+                  "Notice":NoticeID.toString(),
+                  "NoticeUplaodDateTime":"${DateTime.now().toIso8601String()}"
+
+
+               
+                };
+
+
+
+
+
+
+
+
+
+                 
+
+
+
+
+
+
+
+
+
+
+
+
+                    docUser.doc(NoticeID).set(NoticeData).then((value) =>setState((){
+
+
+
+                      loading = false;
+
+
+
+
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => AccessoriesScreen()),
+                // );
+
+
+
+
+
+                    })).onError((error, stackTrace) => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              backgroundColor: Colors.red,
+                        content: const Text('Something Wrong'),
+                        action: SnackBarAction(
+                          label: 'Undo',
+                          onPressed: () {
+                            // Some code to undo the change.
+                          },
+                        ),
+                      )));
+
+
+
+
+
+
+
+
+
+
+                  }, child: Text("Upload", style: TextStyle(color: Colors.white),), style: ButtonStyle(
+                   
+          backgroundColor: MaterialStatePropertyAll<Color>(Theme.of(context).primaryColor),
+        ),):Text(""),),
+
+
+
+              
+
+
+
+
+                ],
+              )
+            
+            
+            
+            ],
+          ),
+        ),
+        ),
+      
+      
+    );
+  }
+}
+
+
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Color(0xf08f00ff);
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.875,
+        size.width * 0.5, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.9584,
+        size.width * 1.0, size.height * 0.9167);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
